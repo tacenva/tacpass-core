543a94e0c32cc199b38170c718a00b3c4cd6236f
FITurnip <franklinirtr@gmail.com>
2026-09-08 18:05:14 +0700

    base:  integrating access control,  optimzing, preparation for async

--- entity/permissions.go
+++ entity/permissions.go
@@ -16,9 +16,8 @@ const (
 type Permission struct {
 	ID        string    `json:"-" gorm:"type:varchar(26);primaryKey"`
 	Privilege Privilege `json:"privilege" gorm:"type:varchar(10);not null"`
-	// AccessToken   string    `json:"access_token" gorm:"type:text;not null"`
-	PublicKey string `json:"-" gorm:"type:text;not null"`
-	Revoked   bool   `json:"revoked" gorm:"not null;default:false"`
+	PublicKey string    `json:"-" gorm:"type:text;not null"`
+	Revoked   bool      `json:"revoked" gorm:"not null;default:false"`
 
 	Users    []User        `json:"users,omitempty" gorm:"foreignKey:PermissionID"`
 	Accesses []VaultAccess `json:"-" gorm:"foreignKey:PermissionID;references:ID"`
